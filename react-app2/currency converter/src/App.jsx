033a27b82cff7ce53dbc9e9a9ae0eb9258360fea
harshit <harshityadav0927@gmail.com>
2025-01-06 17:36:19 +0530

    4

--- react-app2/currency converter/src/App.jsx
+++ react-app2/currency converter/src/App.jsx
@@ -1,23 +1,28 @@
 import { useState } from 'react'
+import useCurrency from './hooks/useCurrency'
 import './App.css'
+import Input from './components/input'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [currency, setCurrency] = useState('usd')
 
+console.log(useCurrency(currency))
+  
   return (
     <>
     <div className=' shadow-lg shadow-black-500/50 w-3/5 mt-28 place-self-center'>
       <div>
           <h1 className='text-5xl bg-blue-500 min place-self-center'> Currency Converter</h1> 
       </div>  
-      <div>
-        <div className='justify-self-center bg-yellow-500 min-h-20 m-12 '>
-          <input className='min-h-20 bg-green-500 mx-10 p-8' type="text" value={'ygfyjufvhyj'}/>
-          <input className='mx-6 min-h-20 bg-green-500 mx-10 p-8' type="text" value={'ygfyjufvhyj'}/>
+      <div className='pb-10'>
+        <div className='justify-self-center min-h-20 m-12 '>
+          <Input/>
+          <input className='mx-6 min-h-20 border-2 mx-10 p-8' type="text" value={'INR'}/>
         </div>
-        <div className='justify-self-center bg-yellow-500 min-h-20 m-12 '>
-          <input className='min-h-20 bg-green-500 mx-10 p-8' type="text" value={'ygfyjufvhyj'}/>
-          <input className='mx-6 min-h-20 bg-green-500 mx-10 p-8' type="text" value={'ygfyjufvhyj'}/>
+        <div className='justify-self-center min-h-20 m-12'>
+          <input className='min-h-20 border-2 mx-10 p-8' type="text" placeholder='To'/>
+          <input className='mx-6 min-h-20 mx-10 p-8 border-2' type="text" value={'USD'}/>
         </div>
       </div>
     </div>
---
+++ react-app2/currency converter/src/components/input.jsx
@@ -0,0 +1,7 @@
+function Input(){
+    return <>
+    
+    <input className='min-h-20 border-2 mx-10 p-8' type="text" placeholder='From'/>
+    </>
+}
+export default Input;
