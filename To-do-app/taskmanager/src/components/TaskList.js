c72184bb016ff3bf1e9bf71cb3b5a04ec6d78058
harshit <harshityadav0927@gmail.com>
2025-02-04 16:53:11 +0530

    erfvd

--- To-do-app/taskmanager/src/components/TaskList.js
+++ To-do-app/taskmanager/src/components/TaskList.js
@@ -1,15 +1,24 @@
 import  { useContext, useEffect } from 'react'
 import { TaskContext } from '../context/TaskContext'
 
+
 function TaskList() {
 
-  const contexttask = useContext(TaskContext)
-  const ListTasks = contexttask.tasks
+const contexttask = useContext(TaskContext)
+const ListTasks = contexttask.tasks
 
-  
-  
-  
-  
+return (
+  <>
+  <div>
+  {ListTasks.map((task, i) => (
+          <li key={i}>
+            {task}
+          </li>
+        ))}
+  </div>
+  </>
+)
+
 }
 
 export default TaskList
