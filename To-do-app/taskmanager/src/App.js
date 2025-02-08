540766a60646fb195112fb1fde46f9ba84424a26
harshit <harshityadav0927@gmail.com>
2025-02-08 16:39:09 +0530

    empty

--- To-do-app/taskmanager/src/App.js
+++ To-do-app/taskmanager/src/App.js
@@ -1,13 +1,1 @@
-import Input from "./components/Input";
-import TaskList from "./components/TaskList";
-function App() {
-  
-  return (
-      <>
-    <Input></Input>    
-    <TaskList></TaskList>  
-      </>
-  );
-}
 
-export default App;
--- To-do-app/taskmanager/src/index.js
+++ To-do-app/taskmanager/src/index.js
@@ -1,13 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.js'
-import { TaskProvider } from './context/TaskContext.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <TaskProvider>
-      <App />
-    </TaskProvider>
-  </React.StrictMode>
-);
