84a5f08c4e626c57eef05870430548029837ef8f
harshit <harshityadav0927@gmail.com>
2025-02-13 18:02:11 +0530

    n1

--- To-do-app/taskmanager/src/App.js
+++ To-do-app/taskmanager/src/App.js
@@ -1,1 +1,39 @@
+import { useState } from "react"
+import { TodoProvider } from "./context/TodoContext"
 
+
+function App(){
+    const [todos, setTodos] = useState([])
+
+    const addTodo = (todo)=>{
+        setTodos((prev)=> [{id: Date.now(),...todo},...prev])
+    }
+
+    const updateTodo = (id, todo) =>{
+        setTodos((prev)=> prev.map((prevTodo)=> (prevTodo.id === id ? todo : prevTodo)))
+    }
+
+    const deleteTodo = (id)=>{
+        setTodos((prev)=> prev.filter((todo)=>todo.id !== id))
+    }
+
+    return (
+        <TodoProvider value={{todos, addTodo,updateTodo, deleteTodo}}>
+        <div className="Addtodo">
+           <h1>
+            Manage Todo
+           </h1>
+            <input>Add task</input>
+            <button>+</button>
+        </div>
+        <div>
+            <p>
+                task1
+            </p>
+            <p>
+                task2
+            </p>
+        </div>
+        </TodoProvider>
+    )
+}
